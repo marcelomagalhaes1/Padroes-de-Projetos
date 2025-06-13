797db49d4139881e14581c89bddc735e0b64ebb6
Marcelo Magalhães <128628299+marcelomagalhaes1@users.noreply.github.com>
2025-06-12 21:57:17 -0300

    Delete Plataforma de Vídeo directory

--- Plataforma de Vídeo/src/Observable.java
+++
@@ -1,5 +0,0 @@
-public interface Observable {
-    void registerObserver(String genero, Observer observer);
-    void removeObserver(String genero, Observer observer);
-    void notifyObservers(String genero, String titulo);
-}
